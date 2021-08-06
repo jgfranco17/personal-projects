258ccaebb5d5f36acbe0e4a0216ad1abc081c0a9
Chino Franco <chino.franco@gmail.com>
2021-08-07 00:32:42 +0900

    Add files via upload

---
+++ resume-website/website/__init__.py
@@ -0,0 +1,29 @@
+# This initializer program is used to make the entire folder a module. This allows for easier importing
+# syntax and for more convenient structuring of the code.
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "secret"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+
+    from .views import views
+    app.register_blueprint(views, url_prefix="/")
+    create_database(app)
+
+    return app
+
+
+def create_database(app):
+    if not path.exists("website/" + DB_NAME):
+        db.create_all(app=app)
+        print("Created database!")
